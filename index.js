760544611e122e27ef311c2488cff2bc973fd1b6
EIndriksons <edgars@indriksons.lv>
2020-12-30 00:15:53 +0200

    Three principles

---
+++ index.js
@@ -0,0 +1,4 @@
+// *** Three core concepts:
+// * Store - holds the state of your application
+// * Action - describes what happened ex. BUY_CAKE
+// * Reducer - ties the store and actions together and tells what to do or change
