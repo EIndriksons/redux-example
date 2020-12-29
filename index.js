e01270305a8e78872b2816c9833bd6096ddbcab6
EIndriksons <edgars@indriksons.lv>
2020-12-30 00:19:14 +0200

    Three principles

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // *** Three core concepts:
-// * Store - holds the state of your application
-// * Action - describes what happened ex. BUY_CAKE
-// * Reducer - ties the store and actions together and tells what to do or change
+// * STORE - holds the state of your application
+// * ACTION - describes what happened ex. BUY_CAKE
+// * REDUCER - ties the store and actions together and tells what to do or change
+
+// *** Three principles:
+// * 1) Maintain our application state in single object managed by Redux store
+// * 2) Only way to change the state is to emit an action
+// * 3) Write pure reducers - reducer should always return newState from (prevState, action)
+
+// * App -(dispatch)-> Action -> Reducer -> Store -(subscribe)-> App... and so on
